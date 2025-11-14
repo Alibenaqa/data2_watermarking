1282f024c6bb936db5eb56c24d53e74e28c87fc8
Alirrobio <mohamedalibenaqa@gmail.com>
2025-11-14 16:04:41 +0100

    feature:adding the image show

--- fichier_backend.py
+++ fichier_backend.py
@@ -1,58 +1,80 @@
-import string
+# import string
+import numpy as np
+from PIL import Image
+# Charger l'image avec Pillow
+image = Image.open("hitme.png")
 
-def cesar_cipher(text, key, cipher):
-	if type(text) == str and type(key) == int:
-		shift = 1 if cipher else -1
-		list_of_crypted_chars = []
-		for char in text :
-			list_of_crypted_chars.append(chr((ord(char) + shift * key) % 1_114_112))
+# Vérification et informations
+print("Format :", image.format)    # ex: PNG, JPEG
+print("Taille :", image.size)      # (largeur, hauteur)
+print("Mode :", image.mode)        # ex: RGB
+image.show()
 
-		crypted_text = "".join(list_of_crypted_chars)
-		return crypted_text
-	else:
-		raise(TypeError)
+# def to_binary(message):
 
-
-def hack_cesar_cipher(crypted_text, alphabet):
-	if type(crypted_text) == str and type(alphabet) == str:
-		for possible_key in range(0, 1_114_112):
-			possible_uncryption = cesar_cipher(crypted_text, possible_key, cipher=False)
-			if possible_uncryption[0] in alphabet:
-				print(possible_key)
-				print(possible_uncryption)
-				print("_"*20)
-	else:
-		raise(TypeError)
+#     binary = ''.join(format(ord(char), '08b') for char in message)
+#     return binary
 
 
-def vigenere_cipher(text, password, cipher):
-	list_of_crypted_chars = []
-	list_of_keys = [ord(char) for char in password]
+
+# def cesar_cipher(text, key, cipher):
+# 	if type(text) == str and type(key) == int:
+# 		shift = 1 if cipher else -1
+# 		list_of_crypted_chars = []
+# 		for char in text :
+# 			list_of_crypted_chars.append(chr((ord(char) + shift * key) % 1_114_112))
+
+# 		crypted_text = "".join(list_of_crypted_chars)
+# 		return crypted_text
+# 	else:
+# 		raise(TypeError)
+
+
+# def hack_cesar_cipher(crypted_text, alphabet):
+# 	if type(crypted_text) == str and type(alphabet) == str:
+# 		for possible_key in range(0, 1_114_112):
+# 			possible_uncryption = cesar_cipher(crypted_text, possible_key, cipher=False)
+# 			if possible_uncryption[0] in alphabet:
+# 				print(possible_key)
+# 				print(possible_uncryption)
+# 				print("_"*20)
+# 	else:
+# 		raise(TypeError)
+
+
+# def vigenere_cipher(text, password, cipher):
+# 	list_of_crypted_chars = []
+# 	list_of_keys = [ord(char) for char in password]
 	
-	for index, current_char in enumerate(text):
+# 	for index, current_char in enumerate(text):
 		
-		current_key = list_of_keys[index % len(list_of_keys)]
-		current_crypted_char = cesar_cipher(current_char, current_key, cipher)
+# 		current_key = list_of_keys[index % len(list_of_keys)]
+# 		current_crypted_char = cesar_cipher(current_char, current_key, cipher)
 
-		list_of_crypted_chars.append(current_crypted_char)
+# 		list_of_crypted_chars.append(current_crypted_char)
 
-	crypted_text = "".join(list_of_crypted_chars)
+# 	crypted_text = "".join(list_of_crypted_chars)
 
-	return crypted_text
+# 	return crypted_text
 
 
-if __name__ == "__main__":
-	message = "le chocolat est bon"
+# if __name__ == "__main__":
+	# message = "le chocolat est bon"
 
-	crypted_text = cesar_cipher(message, 12, cipher=True) # exo 1
-	print(crypted_text)
+	#crypted_text = cesar_cipher(message, 12, cipher=True) # exo 1
+	#print(crypted_text)
+#
+	#initial_message = cesar_cipher(crypted_text, 12, cipher=False) # exo 2
+	#print(initial_message == message)
 
-	initial_message = cesar_cipher(crypted_text, 12, cipher=False) # exo 2
-	print(initial_message == message)
+	#hack_cesar_cipher(crypted_text, alphabet=string.printable) # exo3
 
-	hack_cesar_cipher(crypted_text, alphabet=string.printable) # exo3
+	##crypted_message = vigenere_cipher(text=message, password="Azerty12345!", cipher=True)
+	#print(crypted_message)
+	#initial_message = vigenere_cipher(text=crypted_message, password="Azerty12345!", cipher=False)
+	#print(initial_message)
 
-	crypted_message = vigenere_cipher(text=message, password="Azerty12345!", cipher=True)
-	print(crypted_message)
-	initial_message = vigenere_cipher(text=crypted_message, password="Azerty12345!", cipher=False)
-	print(initial_message)
+	#crypted_message = vigenere_cipher(text=message, password="Azerty12345!", cipher=True)
+	#print(crypted_message)
+	#initial_message = vigenere_cipher(text=crypted_message, password="Azerty12345!", cipher=False)
+	#print(initial_message)
